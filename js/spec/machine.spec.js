8a4107aceca9cda294c95b2578fba11e4d19ef9d
Chris Buchert <chris@smartystreets.com>
2017-10-07 13:56:01 -0600

    Refactored the tests.

--- js/spec/machine.spec.js
+++ js/spec/machine.spec.js
@@ -2,25 +2,27 @@ const expect = require("expect");
 
 describe ("A slot machine", function () {
 	const Machine = require("../machine");
+	let machine;
+	const mockUi = {};
+	const reelSymbols = [1, 2, 3, 4, 5];
 
-	it ("can be run.", function () {
-		let machine = new Machine();
-
-		expect(machine.hasOwnProperty("run")).toBe(true);
+	beforeEach(function () {
+		machine = new Machine(reelSymbols, mockUi);
 	});
 
 	it ("generates three reels when run.", function () {
-		const reelSymbols = [1, 2, 3, 4, 5];
-		let machine = new Machine(reelSymbols);
 		let reels = machine.run();
 
 		expect(reels.length).toEqual(3);
+		console.log(reels);
 	});
 
 	it ("has a ui.", function () {
-		const mockUi = {};
-		let machine = new Machine([], mockUi);
 
 		expect(JSON.stringify(machine.ui)).toEqual(JSON.stringify(mockUi));
 	});
+
+	// it ("slices its generated arrays into 3 symbol frames", function () {
+	//
+	// });
 });
